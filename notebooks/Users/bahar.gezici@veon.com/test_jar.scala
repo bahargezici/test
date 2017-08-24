ba570ad4aa5803e0b3c303589f2c41b6a69a3f13
bahargezici <gezici.bahar@gmail.com>
2017-08-24 13:40:53 +0200

    datetime is added

--- notebooks/Users/bahar.gezici@veon.com/test_jar.scala
+++ notebooks/Users/bahar.gezici@veon.com/test_jar.scala
@@ -1,2 +1,8 @@
 // Databricks notebook source
 import databricks.examples.ExampleClass
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC from datetime import datetime, timedelta
+// MAGIC print(datetime.today())
